c8abf8fca7bc5d378d5032326d0e61bf2504cad0
Jiff Slater <jiff@tankwell>
2022-04-10 17:16:28 +0100

    Add my blocks.h file and remove it from .gitignore.

---
+++ blocks.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30, 0},
+	{"", "cat /sys/class/power_supply/BAT0/status",	120, 0},
+	{"", "cat /sys/class/power_supply/BAT0/capacity", 120, 0},
+	{"Net: ", "iwctl station wlan0 show | grep 'Connected network' | awk '{ print $3 }'", 120, 0},
+	{"", "date '+%A %F %R'",					60,		0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
