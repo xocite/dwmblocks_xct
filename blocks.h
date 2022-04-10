7e2b4286eda81397cb02a577ce00fa2483b6a1dd
Jiff Slater <jiff@tankwell>
2022-04-10 17:17:11 +0100

    Change Wi-Fi reference to use iw instead of iwctl.
    Reason: iwctl now requires the user to be in the netdev group to communicate with iwd over DBUS.

--- blocks.h
+++ blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{"", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	30, 0},
 	{"", "cat /sys/class/power_supply/BAT0/status",	120, 0},
 	{"", "cat /sys/class/power_supply/BAT0/capacity", 120, 0},
-	{"Net: ", "iwctl station wlan0 show | grep 'Connected network' | awk '{ print $3 }'", 120, 0},
+	{"Net: ", "iw dev wlan0 info | grep ssid | awk '{print $2}'", 120, 0},
 	{"", "date '+%A %F %R'",					60,		0},
 };
 
